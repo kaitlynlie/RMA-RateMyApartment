5fb36726ac2e6c84cd7a40c71d4b7ae590e4ee4a
kaitlyn li <kaitlynli0903@gmail.com>
2023-09-02 19:55:31 -0700

    finished homepage, added map

--- components/apartments.jsx
+++ components/apartments.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { ImageBackground, View, ScrollView, SafeAreaView , StyleSheet, Text} from 'react-native';
 import styles from './apartments.style';
+import MapView from 'react-native-maps';
 
 
 const Apartments = () => {
   return(
-      <View>
-       <Text numberOfLines = {2} style={styles.welcomeMessage}>Rate My Apartment</Text>
-      </View>
+    <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  </View>
   )
 }
 
